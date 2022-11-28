fbf2609cb082f358cae93afd6c8ccae71ad39c9f
jyoti <jyoti@DESKTOP-TODJAB7>
2022-11-28 23:52:25 +0530

    added class firstpgm.java

---
+++ FreshPracticeJava/src/test/java/FreshPracticeJava/FreshPracticeJava/firstpgm.java
@@ -0,0 +1,12 @@
+package FreshPracticeJava.FreshPracticeJava;
+
+public class firstpgm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		
+
+	}
+
+}
